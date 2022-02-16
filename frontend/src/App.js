3acc9cb85a3caa8e3cc8fb6685785a77200e2dd9
Nzaro22 <nzaro4@gmail.com>
2022-02-16 11:29:53 +0200

    nadav

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { useReducer } from 'react'
 import dataContext from './Context/dataContext.js';
 import { Reducer, initialState } from './Reducer/dataReducer.js'
 import "./App.css";
+import Home from './pages/Home.jsx';
 
 function App() {
   const [state, dispatch] = useReducer(Reducer, initialState)
@@ -9,7 +10,7 @@ function App() {
   return (
     <>
       <dataContext.Provider value={{ state, dispatch }}>
-
+        <Home/>
       </dataContext.Provider></>
   );
 }
