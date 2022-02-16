b1fd4665643c16524fa06ae9668ba2a62117fa34
anat mersha <anatmersha@gmail.com>
2022-02-16 22:47:04 +0200

    anat: check

--- frontend/src/components/Conversation.jsx
+++ frontend/src/components/Conversation.jsx
@@ -1,36 +1,36 @@
-import axios from "axios";
-import { useEffect, useState, useContext } from "react";
+// import axios from "axios";
+// import { useEffect, useState, useContext } from "react";
 
-const Conversation = ({ convo }) => {
-    const [users, setUsers] = useState(null);
+// const Conversation = ({ convo }) => {
+//     const [users, setUsers] = useState(null);
 
-// לרוץ על כל החברים בקב צ'אט
-    useEffect(()=> {
-        const getUser = () => {
-        // get all other chat members id`s
-        const friends = currChat?.members?.filter((item)=> item !== currUser?._id)
-        // 
-        const chatFriends = users?.forEach(user => {
-        user._id === friends ? chatFriends?.push(user._id) : ""
-        });
-        setUsers(chatFriends);
-        console.log(currChat);
-        }
-        getUser();
-    },[currUser, convo])
+// // לרוץ על כל החברים בקב צ'אט
+//     useEffect(()=> {
+//         const getUser = () => {
+//         // get all other chat members id`s
+//         const friends = currChat?.members?.filter((item)=> item !== currUser?._id)
+//         // 
+//         const chatFriends = users?.forEach(user => {
+//         user._id === friends ? chatFriends?.push(user._id) : ""
+//         });
+//         setUsers(chatFriends);
+//         console.log(currChat);
+//         }
+//         getUser();
+//     },[currUser, convo])
 
-    // מציגה את תמונות המשתמשים שבצ'אט הנוכחי
-    return(
-        <>
-        {users ? users.map((it, i)=> {
-                return(
-                    <div key={i} className="convo">
-                        <img className="convoImg" src={it?.img} alt=""/>
-                    </div>
-                )
-        }): ""}       
-        </>
-    )
-}
+//     // מציגה את תמונות המשתמשים שבצ'אט הנוכחי
+//     return(
+//         <>
+//         {users ? users.map((it, i)=> {
+//                 return(
+//                     <div key={i} className="convo">
+//                         <img className="convoImg" src={it?.img} alt=""/>
+//                     </div>
+//                 )
+//         }): ""}       
+//         </>
+//     )
+// }
 
-export default Conversation;
+// export default Conversation;
