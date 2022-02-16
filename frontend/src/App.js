b794a0d2c8754fb485ae97951634568ba2d765e7
beri <barikassa01@gmail.com>
2022-02-16 17:08:35 +0200

    bari:Login/register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,17 @@
-import React, { useReducer } from 'react'
-import dataContext from './Context/dataContext.js';
-import { Reducer, initialState } from './Reducer/dataReducer.js'
+import React, { useReducer } from "react";
+import dataContext from "./Context/dataContext.js";
+import { Reducer, initialState } from "./Reducer/dataReducer.js";
 import "./App.css";
 
+
 function App() {
-  const [state, dispatch] = useReducer(Reducer, initialState)
+  const [state, dispatch] = useReducer(Reducer, initialState);
+  const [auth, setAuth] = useReducer();
 
   return (
     <>
-      <dataContext.Provider value={{ state, dispatch }}>
-
-      </dataContext.Provider></>
+      <dataContext.Provider value={{ state, dispatch }}></dataContext.Provider>
+    </>
   );
 }
 
