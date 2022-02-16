abb55278f62d78c7c0b8a4a5df2e3601b5215d7e
Nzaro22 <nzaro4@gmail.com>
2022-02-16 22:28:22 +0200

    nadav

--- frontend/src/components/AutoCompleteLocation.jsx
+++ frontend/src/components/AutoCompleteLocation.jsx
@@ -13,13 +13,14 @@ export default function AutoCompleteLocation() {
     console.log(value);
   }
 
-  return (<
-    <GeoapifyContext apiKey="cb3099bb7a604bb0b86052dbe0ad8b65">
-      <GeoapifyGeocoderAutocomplete
-        placeholder="Enter address here"
-        placeSelect={onPlaceSelect}
-        suggestionsChange={onSuggectionChange}
-      />
-    </GeoapifyContext>
+  return (
+    // <GeoapifyContext apiKey="cb3099bb7a604bb0b86052dbe0ad8b65">
+    //   <GeoapifyGeocoderAutocomplete
+    //     placeholder="Enter address here"
+    //     placeSelect={onPlaceSelect}
+    //     suggestionsChange={onSuggectionChange}
+    //   />
+    // </GeoapifyContext>
+    <></>
   );
 }
