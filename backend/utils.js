a5b1f60cc45fdf81fa84605c2ae51e7a3f713316
Brano <asbrano4@gmail.com>
2022-02-16 14:11:23 +0200

    brano add fuction

--- backend/utils.js
+++ backend/utils.js
@@ -6,10 +6,29 @@ const MongoClient = mongodb.MongoClient
 const ObjectId = mongodb.ObjectId
 const url = process.env.MONGO_URI
 const client = MongoClient.connect(url)
+const DB = 'mycity'
+const usersCollection = 'users'
+
+
+function usersData(req, res) {
+    client
+      .then((data) => {
+        const database = data.db(DB);
+        database
+          .collection(usersCollection)
+          .find({})
+          .toArray()
+          .then((docs) => {
+            res.send(docs)
+          });
+      })
+      .catch((err) => {
+        res.status(400).send({ error: { message: 'Bad Request' } });
+        throw err
+      });
+  }
 
 
 
-
-
-const Database = { }
+const Database = { usersData }
 export default Database
