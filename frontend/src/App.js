470a2acfb7adc96a0f6a1bf29ecb11719712de6a
anat mersha <anatmersha@gmail.com>
2022-02-16 18:59:50 +0200

    anat: checking conflict

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { Reducer, initialState } from './Reducer/dataReducer.js'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import "./App.css";
 import Home from './pages/Home.jsx';
+import Chat from './pages/Chat.jsx';
 
 function App() {
   const [state, dispatch] = useReducer(Reducer, initialState)
@@ -13,8 +14,9 @@ function App() {
       <Router>
         <>
 
-      <Routes>
+      <Routes> 
       <Route path="/Home" element={<Home/>}/>
+      {/* <Route path="/Chat" element={<Chat/>}/> */}
 
         </Routes>
         </>
