d23d8a91e3f5874cdb7ea6037debc82fb59a33d1
Brano <asbrano4@gmail.com>
2022-02-16 16:29:54 +0200

    brano

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv"
-import userRoute from './routing/users.js'
+import userRoute from './routes/users.js'
 dotenv.config()
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
+
 app.use('/users',userRoute)
+
 
   
 app.get("*", (req, res) => {
