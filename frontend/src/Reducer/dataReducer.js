f6e5a944e013ea789d2464be2bb63cc99dff12ed
anat mersha <anatmersha@gmail.com>
2022-02-17 15:23:53 +0200

    anat

--- frontend/src/Reducer/dataReducer.js
+++ frontend/src/Reducer/dataReducer.js
@@ -24,19 +24,7 @@ export const initialState = {
   users: isAuth,
   currRoom: null,
   auth: null,
-<<<<<<< HEAD
-  currUser: {
-    _id: "ObjectId(620dac5d90dc9dcf97bc3d24)",
-    firstName: "גל",
-    lastName: "אטנאו",
-    idCard: "111",
-    city: "בתים",
-    email: "falCc@gal.com",
-    entity: 'user',
-},
-=======
   currUser: currentUser,
->>>>>>> origin/main
   reports:[],
   convos: [],
   messages: [],
